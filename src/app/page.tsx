2d30dfb82e13a4ad3c20305f27a0b6daf2ed5060
Waldo Lavaut <waldothedeveloper@gmail.com>
2024-08-10 00:18:00 -0400

    framer motion installed and some magicui components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
-  return (
-    <main>
-      <p className="text-black">Clean Page</p>
-      <Button>Click me</Button>
-    </main>
-  );
+  return <main></main>;
 }
