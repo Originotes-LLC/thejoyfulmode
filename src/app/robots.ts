84d5fb8595713cc248a82d04e0fc231d5cd743c9
Waldo Lavaut <waldothedeveloper@gmail.com>
2024-08-23 14:59:26 -0400

    fixed link to sitemaps with redirect in robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/private/",
     },
-    sitemap: "https://thejoyfulmode.com/sitemap.xml",
+    sitemap: "https://www.thejoyfulmode.com/sitemap.xml",
   };
 }
