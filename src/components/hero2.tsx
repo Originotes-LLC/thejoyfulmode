57cbf1a5fb16ce03e20c1bc0e4978b4da57028fd
Waldo Lavaut <waldothedeveloper@gmail.com>
2024-08-26 11:42:07 -0400

    added lawyer image, and social proof statistics

--- src/components/hero2.tsx
+++ src/components/hero2.tsx
@@ -1,5 +1,10 @@
+import { ArrowUpRightIcon, PhoneIcon } from "@heroicons/react/24/outline";
+
+import { Button } from "@/components/ui/button";
+import { Circle } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { StatsHero } from "./stats-hero";
 import lawyer from "./lawyer-transparent.png";
 
 export function Hero() {
@@ -11,22 +16,36 @@ export function Hero() {
             Hey, 👋🏻 Law Firm. <br /> Your Next Big Case
             <span className="text-accent"> Starts Here</span>.
           </h1>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
+          <p className="mt-6 text-lg leading-8 text-foreground font-medium">
             Expert SEO, Marketing, Web Design & Development tailored for{" "}
             <span className="text-accent">Law Firms {` `}</span>
             that want to be found, not forgotten.
           </p>
           <div className="mt-10 flex items-center gap-x-6">
-            <Link
-              href="/schedule-call"
-              className="rounded-md bg-background px-3.5 py-2.5 text-sm font-semibold text-foreground shadow-sm hover:bg-accent hover:text-background focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary"
-            >
-              Get Started
-            </Link>
+            <div className="flex items-center gap-x-6 lg:flex-shrink-0">
+              <Link className="group" href="/schedule-call">
+                <Button className="rounded-[8px] inline-flex items-center p-6 text-sm bg-foreground lg:text-base font-medium text-background shadow-sm ring-2 ring-inset ring-foreground group-hover:bg-accent group-hover:text-white group-hover:ring-accent">
+                  Get Started Now
+                  <ArrowUpRightIcon
+                    aria-hidden="true"
+                    className="ml-5 size-5 group-hover:text-white text-background"
+                  />
+                </Button>
+              </Link>
+            </div>
           </div>
         </div>
-        <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
-          <div className="relative max-w-xl flex-none sm:max-w-3xl lg:max-w-none">
+        <div className="relative mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none">
+          <div className="absolute lg:top-32 overflow-hidden">
+            <div className="transform translate-x-12 translate-y-12 md:transform-none">
+              <Circle className="text-sky-100 h-[800px] w-[800px] md:w-[1600px] md:h-[1600px] bg-accent/50 rounded-full" />
+            </div>
+          </div>
+          {/* section non sense icons */}
+          <div className="absolute top-20 lg:top-72 lg:transform lg:translate-x-32">
+            <StatsHero />
+          </div>
+          <div className="max-w-xl flex-none sm:max-w-3xl lg:max-w-none z-50">
             <div className="-m-2 p-2 lg:-m-4 lg:p-4">
               <Image
                 alt="A young lawyer feeling empowered by the joyful mode services"
---
+++ src/components/stats-hero.tsx
@@ -0,0 +1,92 @@
+import { ArrowDownIcon, ArrowUpIcon } from "@heroicons/react/20/solid";
+import { ChartNoAxesCombined, Globe, UsersRound } from "lucide-react";
+
+const stats = [
+  {
+    id: 1,
+    name: "Client Conversions",
+    stat: "78%",
+    icon: Globe,
+    change: "11",
+    changeType: "increase",
+  },
+  {
+    id: 2,
+    name: "Bounce Rate",
+    stat: "25%",
+    icon: UsersRound,
+    change: "5",
+    changeType: "decrease",
+  },
+  {
+    id: 3,
+    name: "Marketing Campaign Reach",
+    stat: "2,560",
+    icon: ChartNoAxesCombined,
+    change: "15",
+    changeType: "increase",
+  },
+];
+
+function classNames(...classes: (string | boolean | undefined)[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export function StatsHero() {
+  return (
+    <div>
+      <dl className="grid grid-cols-1">
+        {stats.map((item) => (
+          <div
+            key={item.id}
+            className="relative overflow-hidden px-4 pt-5 sm:px-6 sm:pt-6"
+          >
+            <dt>
+              <div className="absolute rounded-md bg-background/80 p-2">
+                <item.icon
+                  aria-hidden="true"
+                  className="size-5 md:size-6 text-foreground"
+                />
+              </div>
+              <p className="ml-14 truncate text-sm font-medium text-foreground/80">
+                {item.name}
+              </p>
+            </dt>
+            <dd className="ml-14 flex items-baseline">
+              <p className="font-medium text-foreground text-sm">{item.stat}</p>
+              <p
+                className={classNames(
+                  item.changeType === "increase"
+                    ? "text-green-700"
+                    : "text-red-600",
+                  "ml-2 flex items-baseline text-xs font-semibold"
+                )}
+              >
+                {item.changeType === "increase" ? (
+                  <ArrowUpIcon
+                    aria-hidden="true"
+                    className="size-4 flex-shrink-0 self-center text-green-700"
+                  />
+                ) : (
+                  <ArrowDownIcon
+                    aria-hidden="true"
+                    className="size-4 flex-shrink-0 self-center text-red-500"
+                  />
+                )}
+
+                <span className="sr-only">
+                  {" "}
+                  {item.changeType === "increase"
+                    ? "Increased"
+                    : "Decreased"}{" "}
+                  by{" "}
+                </span>
+                {item.change}
+              </p>
+            </dd>
+          </div>
+        ))}
+      </dl>
+    </div>
+  );
+}
