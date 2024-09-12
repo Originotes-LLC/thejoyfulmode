ff80fc157d9f23723727736a45d789c0a21f7023
Waldo Lavaut <waldothedeveloper@gmail.com>
2024-09-12 18:11:10 -0400

    changed the lawyer image cover

--- src/app/(routes)/law-academy/empty-blog.tsx
+++ src/app/(routes)/law-academy/empty-blog.tsx
@@ -1,26 +1,23 @@
 import Image from 'next/image';
-import studyLawyer from '@/assets/images/studying_lawyer.jpeg';
+import studyLawyer from '@/assets/images/two_people.jpeg';
 
 //
 export function EmptyBlog() {
   return (
-    <div className='relative min-h-screen px-6 py-96 sm:px-12 sm:py-40 lg:px-16'>
+    <div className='relative px-6 py-32 sm:px-12 sm:py-40 lg:px-16 xl:py-96'>
       <div className='absolute inset-0 overflow-hidden'>
         <Image
           alt='lawyer studying the best seo tips from the joyful mode law academy'
           src={studyLawyer}
-          className='size-full object-cover xl:object-left'
+          className='size-full object-cover'
         />
       </div>
-      <div
-        aria-hidden='true'
-        className='absolute inset-0 bg-gray-900 bg-opacity-50'
-      />
-      <div className='relative mx-auto flex max-w-3xl flex-col items-center text-center'>
+      <div aria-hidden='true' className='absolute inset-0 bg-foreground/50' />
+      <div className='relative flex flex-col items-start xl:flex-row'>
         <h2 className='text-h1Small text-white xl:text-h1Large'>
           The Joyful Law Academy
         </h2>
-        <p className='mt-5 text-2xl font-normal leading-loose text-white xl:mt-12 xl:text-4xl xl:leading-10'>
+        <p className='mt-5 text-2xl font-normal leading-loose text-white xl:mt-32 xl:text-4xl xl:leading-10'>
           We're working hard to bring you the best SEO tips, marketing
           strategies, and web design & development tailored for law firms that
           want to be noticed, not ignored. Stay tuned!
